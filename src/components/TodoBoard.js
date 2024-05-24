8ed07d040beb676052d6cf183e5925e4ecd2962d
sinhey <sinheyyy@gmail.com>
2024-05-24 20:51:31 +0900

    first commit

--- src/components/TodoBoard.js
+++ src/components/TodoBoard.js
@@ -1,11 +1,16 @@
 import React from "react";
+import TodoItem from "./TodoItem";
 
-const TodoBoard = () => {
+const TodoBoard = ({ todoList, updateTask, deleteTask }) => {
   return (
     <div>
       <h2>Todo List</h2>
+      {todoList.length > 0 ?
+        (todoList.map((item) => <TodoItem item={item} updateTask={updateTask} deleteTask={deleteTask} />))
+        :
+        (<h2>There is no Item to show</h2>)}
       {/* <TodoItem/> will be here once we get the todoList */}
-      <h2>There is no Item to show</h2>
+
     </div>
   );
 };
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-const TodoItem = () => {
+const TodoItem = ({ item, updateTask, deleteTask }) => {
   return (
     <Row>
       <Col xs={12}>
-        <div className={`todo-item`}>
-          <div className="todo-content">밥먹기</div>
-
+        <div className={`todo-item ${item.isComplete ? "item-complete" : ""}`}>
+          <div className="todo-content">{item.task}</div>
           <div>
-            <button className="button-delete">삭제</button>
-            <button className="button-delete">끝남</button>
+            <button className="button-delete" onClick={() => deleteTask(item._id)}> 삭제</button>
+            <button className="button-delete" onClick={() => updateTask(item._id)}>{item.isComplete ? `안끝남` : `끝남`}</button>
           </div>
         </div>
       </Col>
