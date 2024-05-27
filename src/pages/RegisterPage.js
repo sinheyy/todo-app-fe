0ff8e183ead0df0b6809c7dca6e0878f9cc95f23
sinhey <sinheyyy@gmail.com>
2024-05-28 01:37:49 +0900

    오타 수정

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import api from "../utils/api";
-import { seNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const RegisterPage = () => {
     const [name, setName] = useState('');
